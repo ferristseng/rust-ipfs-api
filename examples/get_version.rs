03b0f5d5a9a0e333c6269bd93ff183e205f8376c
Ferris Tseng <ferristseng@fastmail.fm>
2017-10-08 17:31:56 -0400

    add example for getting the ipfs server's version

---
+++ examples/get_version.rs
@@ -0,0 +1,26 @@
+extern crate ipfs_api;
+extern crate tokio_core;
+
+
+use ipfs_api::IpfsClient;
+use tokio_core::reactor::Core;
+
+
+// Creates an Ipfs client, and gets the version of the Ipfs server.
+//
+fn main() {
+    if let Ok(mut core) = Core::new() {
+        println!("connecting to localhost:5001...");
+
+        let client =
+            IpfsClient::new(&core.handle(), "localhost", 5001).expect("expected a valid url");
+        let req = client.version();
+
+        println!(
+            "version: {:?}",
+            core.run(req).expect("expected a valid response")
+        )
+    } else {
+        println!("failed to create event loop");
+    }
+}
