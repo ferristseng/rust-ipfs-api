8b055e583721b833d8528323dc0ad93ec9f5bc00
Ferris Tseng <ferristseng@fastmail.fm>
2017-10-25 23:00:48 -0400

    add license

--- README.md
+++ README.md
@@ -18,3 +18,16 @@ ipfs-api = "0.4.0"
   * Add integration tests for the `go-ipfs` implementation, and `js-ipfs` implementation of the ipfs spec.
   * Explore a higher level API for interacting with IPFS.
   * File system abstraction
+
+## License
+
+Licensed under either of
+
+ * Apache License, Version 2.0, ([LICENSE-APACHE](LICENSE-APACHE) or http://www.apache.org/licenses/LICENSE-2.0)
+ * MIT license ([LICENSE-MIT](LICENSE-MIT) or http://opensource.org/licenses/MIT)
+
+at your option.
+
+### Contribution
+
+Unless you explicitly state otherwise, any contribution intentionally submitted for inclusion in the work by you, as defined in the Apache-2.0 license, shall be dual licensed as above, without any additional terms or conditions.
--- ipfs-api/examples/get_version.rs
+++ ipfs-api/examples/get_version.rs
@@ -1,6 +1,16 @@
+// Copyright 2017 rust-ipfs-api Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+
+extern crate futures;
 extern crate ipfs_api;
 extern crate tokio_core;
 
+use futures::stream::Stream;
 use ipfs_api::IpfsClient;
 use tokio_core::reactor::Core;
 
@@ -15,4 +25,28 @@ fn main() {
     let version = core.run(req).expect("expected a valid response");
 
     println!("version: {:?}", version.version);
+
+    let req = client.refs_local();
+    println!(
+        "{:?}",
+        core.run(req.for_each(|res| Ok(println!("{:?}", res))))
+    );
+
+    let req = client.diag_sys();
+    println!("{}", core.run(req).expect("response"));
+
+    let req = client.dns("ipfs.io", false);
+    let dns = core.run(req).expect("response");
+    println!("{:?}", dns);
+
+    let req = client.file_ls(&dns.path[..]);
+    println!("{:?}", core.run(req).expect("response"));
+
+    /*
+    let req = client.dht_get("QmRJijhiMxQgn7bFP4cBsarHsGMM8g9fLDEE3WtkTXr4Hr");
+    println!(
+        "{:?}",
+        core.run(req.for_each(|res| Ok(println!("{:?}", res))))
+    );
+    */
 }
