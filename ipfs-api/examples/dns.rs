d26081bfc6f5d6c0f17be2f49fcd5f555df9250d
Ferris Tseng <ferris@navapbc.com>
2019-12-23 11:54:07 -0500

    update examples

--- ipfs-api/examples/dns.rs
+++ ipfs-api/examples/dns.rs
@@ -6,37 +6,38 @@
 // copied, modified, or distributed except according to those terms.
 //
 
-use futures::Future;
 use ipfs_api::IpfsClient;
-use tokio::runtime::current_thread::Runtime;
 
 // Creates an Ipfs client, resolves ipfs.io, and lists the contents of it.
 //
-fn main() {
-    println!("connecting to localhost:5001...");
+#[tokio::main]
+async fn main() {
+    eprintln!("connecting to localhost:5001...");
 
     let client = IpfsClient::default();
 
-    let req = client
-        .dns("ipfs.io", false)
-        .and_then(move |dns| {
-            println!("dns resolves to ({})", &dns.path);
-            println!();
+    let dns = match client.dns("ipfs.io", false).await {
+        Ok(dns) => {
+            eprintln!("dns resolves to ({})", &dns.path);
+            eprintln!();
 
-            client.file_ls(&dns.path[..])
-        })
-        .map(|contents| {
-            println!("found contents:");
+            dns
+        }
+        Err(e) => {
+            eprintln!("error resolving dns: {}", e);
+            return;
+        }
+    };
+
+    match client.file_ls(&dns.path[..]).await {
+        Ok(contents) => {
+            eprintln!("found contents:");
             for directory in contents.objects.values() {
                 for file in directory.links.iter() {
-                    println!("[{}] ({} bytes)", file.name, file.size);
+                    eprintln!("[{}] ({} bytes)", file.name, file.size);
                 }
             }
-        })
-        .map_err(|e| eprintln!("{}", e));
-
-    Runtime::new()
-        .expect("tokio runtime")
-        .block_on(req)
-        .expect("successful response");
+        }
+        Err(e) => eprintln!("error listing path: {}", e),
+    }
 }
--- ipfs-api/examples/get_commands.rs
+++ ipfs-api/examples/get_commands.rs
@@ -6,25 +6,23 @@
 // copied, modified, or distributed except according to those terms.
 //
 
-use futures::Future;
 use ipfs_api::{response, IpfsClient};
-use tokio::runtime::current_thread::Runtime;
 
 fn print_recursive(indent: usize, cmd: &response::CommandsResponse) {
     let cmd_indent = " ".repeat(indent * 4);
     let opt_indent = " ".repeat((indent + 1) * 4);
 
-    println!("{}[{}]", cmd_indent, cmd.name);
+    eprintln!("{}[{}]", cmd_indent, cmd.name);
 
     if cmd.options.len() > 0 {
-        println!("{}* options:", cmd_indent);
+        eprintln!("{}* options:", cmd_indent);
         for options in cmd.options.iter() {
-            println!("{}{}", opt_indent, &options.names[..].join(", "));
+            eprintln!("{}{}", opt_indent, &options.names[..].join(", "));
         }
     }
 
     if cmd.subcommands.len() > 0 {
-        println!("{}- subcommands:", cmd_indent);
+        eprintln!("{}- subcommands:", cmd_indent);
         for subcommand in cmd.subcommands.iter() {
             print_recursive(indent + 1, subcommand);
         }
@@ -34,17 +32,14 @@ fn print_recursive(indent: usize, cmd: &response::CommandsResponse) {
 // Creates an Ipfs client, and gets a list of available commands from the
 // Ipfs server.
 //
-fn main() {
-    println!("connecting to localhost:5001...");
+#[tokio::main]
+async fn main() {
+    eprintln!("connecting to localhost:5001...");
 
     let client = IpfsClient::default();
-    let req = client
-        .commands()
-        .map(|commands| print_recursive(0, &commands))
-        .map_err(|e| eprintln!("{}", e));
 
-    Runtime::new()
-        .expect("tokio runtime")
-        .block_on(req)
-        .expect("successful response");
+    match client.commands().await {
+        Ok(commands) => print_recursive(0, &commands),
+        Err(e) => eprintln!("error getting commands: {}", e),
+    }
 }
