3aeb20d68f969216a127585a91eebede4911230f
Ferris Tseng <ferris@navapbc.com>
2019-12-24 17:05:04 -0500

    add example for tailing log

---
+++ ipfs-api/examples/log_tail.rs
@@ -0,0 +1,31 @@
+// Copyright 2019 rust-ipfs-api Developers
+//
+// Licensed under the Apache License, Version 2.0, <LICENSE-APACHE or
+// http://apache.org/licenses/LICENSE-2.0> or the MIT license <LICENSE-MIT or
+// http://opensource.org/licenses/MIT>, at your option. This file may not be
+// copied, modified, or distributed except according to those terms.
+//
+
+use futures::{future, TryStreamExt};
+use ipfs_api::IpfsClient;
+
+// Tails the log of IPFS.
+//
+#[tokio::main]
+async fn main() {
+    eprintln!("connecting to localhost:5001...");
+
+    let client = IpfsClient::default();
+
+    if let Err(e) = client
+        .log_tail()
+        .try_for_each(|line| {
+            println!("{}", line);
+
+            future::ok(())
+        })
+        .await
+    {
+        eprintln!("error getting tail of log: {}", e);
+    }
+}
