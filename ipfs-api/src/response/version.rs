4fdf4a765e4f7ef9f6ad22c76c3e10f875613b16
Ferris Tseng <ferris@navapbc.com>
2020-03-22 18:15:14 -0400

    make version and system fields optional

--- ipfs-api/src/response/version.rs
+++ ipfs-api/src/response/version.rs
@@ -14,11 +14,16 @@ pub struct VersionResponse {
     pub version: String,
     pub commit: String,
     pub repo: String,
-    pub system: String,
-    pub golang: String,
+
+    // Not declared in the IPFS interface spec.
+    pub system: Option<String>,
+
+    // Only for the Go implementation of IPFS.
+    pub golang: Option<String>,
 }
 
 #[cfg(test)]
 mod tests {
     deserialize_test!(v0_version_0, VersionResponse);
+    deserialize_test!(v0_version_1, VersionResponse);
 }
