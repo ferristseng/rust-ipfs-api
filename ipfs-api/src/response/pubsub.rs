54f88dc8886753aef716e1325ea7c164aada30aa
Sionoi <sim.vivier@hotmail.com>
2021-01-27 17:14:06 -0500

    Data & Seqno

--- ipfs-api/src/response/pubsub.rs
+++ ipfs-api/src/response/pubsub.rs
@@ -7,7 +7,14 @@
 //
 
 use crate::response::serde;
-use crate::serde::Deserialize;
+use crate::serde::{Deserialize, Deserializer};
+
+use std::convert::TryFrom;
+use std::convert::TryInto;
+use std::str::FromStr;
+
+use cid::Cid;
+use multibase::Base;
 
 #[derive(Debug, Deserialize)]
 #[serde(rename_all = "PascalCase")]
@@ -27,15 +34,78 @@ pub type PubsubPubResponse = ();
 
 #[derive(Debug, Deserialize)]
 pub struct PubsubSubResponse {
+    //#[serde(deserialize_with = "deserialize_from_field")]
     pub from: Option<String>,
+
+    #[serde(deserialize_with = "deserialize_data_field")]
     pub data: Option<String>,
-    pub seqno: Option<String>,
+
+    #[serde(deserialize_with = "deserialize_seqno_field")]
+    pub seqno: Option<usize>,
 
     #[serde(rename = "topicIDs")]
     pub topic_ids: Option<Vec<String>>,
 
     #[serde(rename = "XXX_unrecognized")]
     pub unrecognized: Option<Vec<u8>>,
+}
+
+/* fn deserialize_from_field<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let from: Option<String> = Deserialize::deserialize(deserializer)?;
+
+    let from = match from {
+        Some(from) => from,
+        None => return Ok(None),
+    };
+
+    let from = Base::decode(&Base::Base64Pad, from).expect("Multibase decoding failed");
+
+    //let from = String::from_utf8(from).expect("Invalid UTF-8");
+
+    //let from = Cid::try_from(from).expect("Deserialize string to CID failed");
+
+    Ok(Some(from))
+} */
+
+fn deserialize_data_field<'de, D>(deserializer: D) -> Result<Option<String>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let data: Option<&str> = Deserialize::deserialize(deserializer)?;
+
+    let data = match data {
+        Some(data) => data,
+        None => return Ok(None),
+    };
+
+    let data = Base::decode(&Base::Base64Pad, data).expect("Multibase decoding failed");
+
+    let data = String::from_utf8(data).expect("Invalid UTF-8");
+
+    Ok(Some(data))
+}
+
+fn deserialize_seqno_field<'de, D>(deserializer: D) -> Result<Option<usize>, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let seqno: Option<&str> = Deserialize::deserialize(deserializer)?;
+
+    let seqno = match seqno {
+        Some(seqno) => seqno,
+        None => return Ok(None),
+    };
+
+    let seqno = Base::decode(&Base::Base64Pad, seqno).expect("Multibase decoding failed");
+
+    let seqno = seqno.try_into().expect("Not 64 bits");
+
+    let seqno = usize::from_be_bytes(seqno);
+
+    Ok(Some(seqno))
 }
 
 #[cfg(test)]
